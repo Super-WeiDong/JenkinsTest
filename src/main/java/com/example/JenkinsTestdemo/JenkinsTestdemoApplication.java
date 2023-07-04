7f26ff38209996b63a88c6705f7aad6002972302
Super-WeiDong <weidongwang@brandeis.edu>
2023-07-04 00:01:37 -0400

    first commit

---
+++ src/main/java/com/example/JenkinsTestdemo/JenkinsTestdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsTestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JenkinsTestdemo/JenkinsTestdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsTestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
